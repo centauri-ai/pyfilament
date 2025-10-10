486b4e7d6026b37f626b3b12e3b62636cbfcff4f
jameswu1991 <james@centauri-ai.tech>
2025-10-10 10:04:48 -0700

    avoid setting up logging twice

--- backend/filament/setup_logging.py
+++ backend/filament/setup_logging.py
@@ -1,5 +1,7 @@
 import logging
 
+_is_setup = False
+
 
 class UnixTimeFormatter(logging.Formatter):
     def formatTime(self, record, datefmt=None):
@@ -8,12 +10,16 @@ class UnixTimeFormatter(logging.Formatter):
 
 
 def setup_logging():
+    global _is_setup
+    if _is_setup:
+        return
     formatter = UnixTimeFormatter(fmt='%(asctime)s [%(levelname)s] %(name)s: %(message)s')
     handler = logging.StreamHandler()
     handler.setFormatter(formatter)
     rootLogger = logging.getLogger()
     rootLogger.setLevel(logging.DEBUG)
     rootLogger.handlers = [handler]  # Replace any existing handlers
+    _is_setup = True
 
 
 setup_logging()
