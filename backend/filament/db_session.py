73eb9a9db015107cfd3ccd66cf7d446e331f3259
jameswu1991 <james@centauri-ai.tech>
2026-02-25 19:45:44 -0800

    fix sessions not terminating safely, enable async pools

--- backend/filament/db_session.py
+++ backend/filament/db_session.py
@@ -2,9 +2,9 @@ import os
 from contextlib import asynccontextmanager
 from urllib.parse import urlparse
 
+import anyio
 from dotenv import load_dotenv
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
-from sqlalchemy.pool import NullPool
 
 load_dotenv()
 DATABASE_URL = os.getenv('FILAMENT_DB_URI', 'sqlite://filament.db')
@@ -26,13 +26,14 @@ def convert_to_async_url(url):
 
 # use a null pool because we don't want to recycle connections
 # engine = create_async_engine(convert_to_async_url(DATABASE_URL), poolclass=NullPool, echo_pool='debug')
-engine = create_async_engine(convert_to_async_url(DATABASE_URL), poolclass=NullPool)
+engine = create_async_engine(convert_to_async_url(DATABASE_URL), pool_size=10, max_overflow=100)
 AsyncSession = async_sessionmaker(bind=engine)
 
 
 @asynccontextmanager
 async def async_session_scope(commit=True, autoflush=True):
-    async with AsyncSession(autoflush=autoflush) as session:
-        yield session
-        if commit:
-            await session.commit()
+    with anyio.CancelScope(shield=True):
+        async with AsyncSession(autoflush=autoflush) as session:
+            yield session
+            if commit:
+                await session.commit()
