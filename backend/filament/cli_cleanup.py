3371e274e06118d81a92d4e39dafe08066921766
jameswu1991 <james@centauri-ai.tech>
2026-04-20 13:51:21 -0700

    skip root tasks

--- backend/filament/cli_cleanup.py
+++ backend/filament/cli_cleanup.py
@@ -25,9 +25,11 @@ setup_logging()
 logger = logging.getLogger(__name__)
 
 
-async def main(stonith_max_heartbeat_seconds: int = 60 * 60, delete_old_task_runs_days: int = 30):
+async def main(
+    stonith_max_heartbeat_seconds: int = 60 * 60, delete_old_task_runs_days: int = 30, skip_root_tasks: bool = True
+):
     await stonith(max_heartbeat_seconds=stonith_max_heartbeat_seconds)
-    await delete_old_task_runs(days=delete_old_task_runs_days)
+    await delete_old_task_runs(days=delete_old_task_runs_days, skip_root_tasks=skip_root_tasks)
 
 
 async def stonith(max_heartbeat_seconds: int, batch_size: int = 100):
@@ -59,14 +61,13 @@ async def stonith(max_heartbeat_seconds: int, batch_size: int = 100):
                 break
 
 
-async def delete_old_task_runs(days: int = 30, batch_size: int = 100):
+async def delete_old_task_runs(days: int = 30, batch_size: int = 100, skip_root_tasks: bool = True):
     while True:
         async with async_session_scope() as session:
-            statement = (
-                select(TaskRun)
-                .where(TaskRun.created_at < datetime.now(timezone.utc) - timedelta(days=days))
-                .limit(batch_size)
-            )
+            statement = select(TaskRun).where(TaskRun.created_at < datetime.now(timezone.utc) - timedelta(days=days))
+            if skip_root_tasks:
+                statement = statement.where(TaskRun.parent_task_uuid.is_not(None))
+            statement = statement.limit(batch_size)
             task_runs = (await session.execute(statement)).scalars().all()
             logger.info(f'Found {len(task_runs)} task runs to delete')
             any_age = None
